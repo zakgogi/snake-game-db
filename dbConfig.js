75991022e40dece73534ac7bfa0dfb247145e0ba
Zak Gogi <zakgogi1@gmail.com>
2021-07-23 14:03:16 +0100

    changed pooling

--- dbConfig.js
+++ dbConfig.js
@@ -1,17 +1,16 @@
 const { Pool } = require("pg");
 
 let pool;
-if (process.env.NODE_ENV === 'production') {
-	pool = new Pool({
+if (process.env.NODE_ENV === 'development'){
+  pool = new Pool();
+} else {
+  pool = new Pool({
 		connectionString: process.env.DATABASE_URL,
 		ssl: {
 			rejectUnauthorized: false,
 		},
 	});
-} else if (process.env.NODE_ENV === 'development') {
-	pool = new Pool();
 }
-
 // const pool = new Pool();
 
 module.exports = pool;
