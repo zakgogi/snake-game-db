4b474a0a8db70f5f8ebd7baa8d0abd7d01884be0
Zak Gogi <zakgogi1@gmail.com>
2021-07-23 13:40:23 +0100

    heroku stuff

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,16 +1,11 @@
 const { Pool } = require("pg");
 
-let pool;
-if (process.env.NODE_ENV === 'production') {
-	pool = new Pool({
-		connectionString: process.env.DATABASE_URL,
-		ssl: {
-			rejectUnauthorized: false,
-		},
-	});
-} else if (process.env.NODE_ENV === 'development') {
-	pool = new Pool();
-}
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 
 // const pool = new Pool();
